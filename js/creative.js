3ca4cf81060d5e48a0f5c53d0cbee974548e00be
Harjyot Singh <harjyot7@gmail.com>
2019-02-02 17:50:14 +0000

    Changes include: 1. New logo 2. Fresh design & fonts 3. Picture of platform

--- js/creative.js
+++ js/creative.js
@@ -1,26 +1,26 @@
-$(function() {
-  $.scrollify({
-    section: ".section",
-    scrollbars: true,
-    before:function(i,panels) {
-      var ref = panels[i].attr("data-section-name");
-      $(".pagination .active").removeClass("active");
-      $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-    },
-    afterRender:function() {
-      var pagination = "<ul class=\"pagination\">";
-      var activeClass = "";
-      $(".section").each(function(i) {
-        activeClass = "";
-        if(i===0) {
-          activeClass = "active";
-        }
-        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name") + "</span></a></li>";
-      });
+// $(function() {
+//   $.scrollify({
+//     section: ".section",
+//     scrollbars: true,
+//     before:function(i,panels) {
+//       var ref = panels[i].attr("data-section-name");
+//       $(".pagination .active").removeClass("active");
+//       $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+//     },
+//     afterRender:function() {
+//       var pagination = "<ul class=\"pagination\">";
+//       var activeClass = "";
+//       $(".section").each(function(i) {
+//         activeClass = "";
+//         if(i===0) {
+//           activeClass = "active";
+//         }
+//         pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name") + "</span></a></li>";
+//       });
 
-      pagination += "</ul>";
-      $(".splash").append(pagination);
-      $(".pagination a").on("click",$.scrollify.move);
-    }
-  });
-});
+//       pagination += "</ul>";
+//       $(".splash").append(pagination);
+//       $(".pagination a").on("click",$.scrollify.move);
+//     }
+//   });
+// });
